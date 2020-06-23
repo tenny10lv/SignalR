50943dcb69d67e22a31e4d3533a63299bea49ef3
ThapLV <quangthap9x@gmail.com>
2020-06-23 21:06:08 +0700

    init

---
+++ SignalR/Hubs/ServerHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ServerHub : Hub
+    {
+        public void SendDataToServer(string data)
+        {
+            Clients.All.getDataFromServer(data);
+        }
+      
+    }
+}
